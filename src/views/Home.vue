4399ecc951196b71b8b4742e1e5992a5f9e8f073
msjones3 <cheryl@realsocial.com.au>
2022-02-21 22:05:23 +1000

    finish home section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,14 @@
 <template>
-  <div class="mt-60 mx-9 max-w-xl">
+  <div class="mt-60 mx-auto sm:mx-20 xl:mx-40 max-w-xs sm:max-w-xl">
     <h1 class="text-7xl text-white" >Cheryl Jones</h1>
-    <h3 class="font-nunito uppercase text-lg text-indigo-500 mt-4">web developer</h3>
+    <h3 class="font-nunito uppercase text-lg text-indigo-500 tracking-wide mt-4">web developer</h3>
     <p class="text-gray-100 mt-8">Hi! I'm a web developer and teacher based in Queensland, Australia. I love helping people find creative solutions to problems, and providing a user experience that is accessible and easy to use.
 </p>
-    <button class="btn mt-8">Contact me</button>
-    <button class="bg-transparent border-2 ml-10 border-indigo-500 text-indigo-500 rounded-full h-10 w-10">hey there</button>
+<div class="flex items-center gap-9 mt-14">
+      <button class="btn">Contact me</button>
+    <button class="bg-transparent border-2 hover:bg-indigo-700 hover:border-transparent border-indigo-500 text-indigo-500 rounded-full h-14 w-14"><i class="fa-solid mt-2 fa-chevron-down animate-bounce text-indigo-500 hover:text-bg-gray-800"></i></button>
+</div>
+
   </div>
 </template>
 
