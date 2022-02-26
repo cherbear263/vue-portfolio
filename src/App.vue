c86a05c39efed49d04deb2006e36d258bf2e1343
msjones3 <cheryl@realsocial.com.au>
2022-02-26 23:17:34 +1000

    fix spacing

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 <div class="py-16">
-<nav class="bg-gray-900 top-0 z-10 inset-x-0 fixed h-16 ">
-  <div class="flex justify-between items-center py-5 max-w-screen-xl mx-9 xl:mx-auto">
+<nav class="fixed inset-x-0 top-0 z-10 h-16 bg-gray-900 ">
+  <div class="flex items-center justify-between max-w-screen-xl py-5 mx-9 xl:mx-auto">
     <router-link class="text-white" to="/">Cheryl Jones</router-link>
     <ul class="flex gap-4">
       <a href="/#about" class="text-white hover:text-blue-500">About</a>
@@ -11,16 +11,16 @@
   </div>
 </nav>
 <!-- SIDEBAR -->
-<div class="invisible sm:visible fixed left-0 top-16 z-10 bg-gray-900 w-1/4 h-screen">
+<div class="fixed left-0 z-10 invisible w-1/4 h-screen bg-gray-900 sm:visible top-16">
     <img class="object-contain shadow-inner" src="./assets/2018.png"/>
 
-    <div class="flex mx-auto gap-3 mt-6 justify-center">
-      <a href="#"><img class="h-8 w-8" src="./assets/icons/github.png" alt="github icon" /></a>
-      <a href="#"><img class="h-8 w-8" src="./assets/icons/linkedin.png" alt="linkedin icon" /></a>
+    <div class="flex justify-center gap-3 mx-auto mt-6">
+      <a href="#"><img class="w-8 h-8" src="./assets/icons/github.png" alt="github icon" /></a>
+      <a href="#"><img class="w-8 h-8" src="./assets/icons/linkedin.png" alt="linkedin icon" /></a>
     </div>
 </div>
   <!-- MAIN CONTENT -->
-<div id="main" class="absolute bg-gray-800 w-full sm:w-3/4 top-0 right-0 min-h-screen overflow-y-auto">
+<div id="main" class="absolute top-0 right-0 w-full min-h-screen overflow-y-auto bg-gray-800 sm:w-3/4">
 <router-view/>
 </div>
 </div>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,45 +1,38 @@
 <template>
-<div class="mx-auto sm:mx-20 xl:mx-40 max-w-xs sm:max-w-xl ">
+<div class="max-w-xs mx-auto sm:mx-20 xl:mx-40 sm:max-w-xl ">
   <section id="intro" class="section">
-    <h1 class="mt-60 text-7xl text-white" >Cheryl Jones</h1>
-    <h3 class="font-nunito uppercase text-lg text-indigo-500 tracking-wide mt-4">web developer</h3>
-    <p class="text-gray-100 mt-8">Hi! I'm a web developer and teacher based in Queensland, Australia. I love helping people find creative solutions to problems, and providing a user experience that is accessible and easy to use.
+    <h1 class="mt-40 text-white sm:mt-60 text-7xl" >Cheryl Jones</h1>
+    <h3 class="mt-4 text-lg tracking-wide text-indigo-500 uppercase font-nunito">web developer</h3>
+    <p class="mt-8 text-gray-100">Hi! I'm a web developer and teacher based in Queensland, Australia. I love helping people find creative solutions to problems, and providing a user experience that is accessible and easy to use.
 </p>
     <div class="flex items-center gap-9 mt-14">
       <a class="btn">Contact me</a>
-      <a href="/#about" class="bg-transparent border-2 hover:bg-indigo-700 hover:border-transparent
-    border-indigo-500 text-center  text-indigo-500 rounded-full h-14 w-14">
-      <i class="fa-solid mt-5 fa-chevron-down animate-bounce end 
-    text-indigo-500 hover:text-bg-gray-800">
+      <a href="/#about" class="text-center text-indigo-500 bg-transparent border-2 border-indigo-500 rounded-full hover:bg-indigo-700 hover:border-transparent h-14 w-14">
+      <i class="mt-5 text-indigo-500 fa-solid fa-chevron-down animate-bounce end hover:text-bg-gray-800">
       </i>
       </a>
       </div>
   </section>
   <!-- ABOUT section -->
   <section id="about" class="section">
-    <h1 class="text-5xl text-white sm:mt-20 mt-60">About me</h1>
-    <p class="text-gray-100 mt-8">I am a teacher of digital technologies, multimedia arts, and science.
-      I have taught computer science from grades 7 to 11, across a range of technologies including
+    <h1 class="mt-40 text-5xl text-white">About me</h1>
+    <p class="mt-8 text-gray-100">I am a teacher of digital technologies, multimedia arts, and science.
+      I have taught computer science from grades 7 to 11, using a range of technologies including
       block-based coding, text-based languages such as Python and Javascript, SQL queries and database design,
       as well as using Flask to create a data-driven solutions. </p>
-      <p class="text-gray-100 mt-8"> My background is in Science. I have a B.Sc. in Biochemistry (Hons),
-      and a B.Ed. (secondary). After taking the CS50 course from HarvardX in
-      computer science, I discovered that I really love programming. Since
-      then, I have devoured programming courses and have tried to use my
-      skills to solve problems for friends and coworkers. I just love the
-        creativity and the challenge that coding brings! My more recent projects have used
+      <p class="mt-8 text-gray-100"> My educational background is in Science, though I dabbled a bit in web design
+        when I built an e-commerce site for sewing supplies. I was then asked to teach digital technologies so
+        I took a computer science course from HarvardX in order to upskill. I have been hooked ever since.
+        I love the creativity and the challenge that coding brings, and I enjoy using my skills to solve
+        problems for friends and coworkers. My more recent projects have used
         Python (including Flask and Django), and previously I have
           dabbled in Wordpress. Most recently I have been interested in 
         learning Vue JS (which this site was created with). </p>
 
-  <p class="text-gray-100 mt-8">I'm looking for opportunities to do web
-  development. If you think we could work together, please get in touch! </p>
       <div class="flex items-center gap-9 mt-14">
       <a class="btn">Contact me</a>
-      <a href="/#skills" class="bg-transparent border-2 hover:bg-indigo-700 hover:border-transparent
-    border-indigo-500 text-center  text-indigo-500 rounded-full h-14 w-14">
-      <i class="fa-solid mt-5 fa-chevron-down animate-bounce end 
-    text-indigo-500 hover:text-bg-gray-800">
+      <a href="/#skills" class="text-center text-indigo-500 bg-transparent border-2 border-indigo-500 rounded-full hover:bg-indigo-700 hover:border-transparent h-14 w-14">
+      <i class="mt-5 text-indigo-500 fa-solid fa-chevron-down animate-bounce end hover:text-bg-gray-800">
       </i>
       </a>
       </div>
@@ -47,79 +40,78 @@
   </section>
  
  <!-- SKILLS --> 
-  <section id="skills" class="section mt-80 sm:mt-20 mb-40 md:mb-0">
-    <h1 class="text-5xl text-white ">Skills</h1>
-    <div class="block md:flex md:gap-8 justify-between mt-8">
+  <section id="skills" class="section mt-80 md:mb-0">
+    <h1 class="text-5xl text-white">Skills</h1>
+    <div class="justify-between block mt-8 md:flex md:gap-8">
       <div class="w-3/4">
-        <h3 class="font-nunito uppercase text-lg text-indigo-500 tracking-wide mt-4">Technical Skills</h3>
+        <h3 class="mt-4 text-lg tracking-wide text-indigo-500 uppercase font-nunito">Technical Skills</h3>
         <ul class="">
-          <li class="text-gray-100 mt-1">version control (git)</li>
-          <li class="text-gray-100 mt-1">analytical skills</li>
-          <li class="text-gray-100 mt-1">data flow and wireframing</li>
-          <li class="text-gray-100 mt-1">testing and debugging</li>
-          <li class="text-gray-100 mt-1">api integration</li>
-          <li class="text-gray-100 mt-1">responsive design</li>
+          <li class="mt-1 text-gray-100">version control (git)</li>
+          <li class="mt-1 text-gray-100">analytical skills</li>
+          <li class="mt-1 text-gray-100">data flow and wireframing</li>
+          <li class="mt-1 text-gray-100">testing and debugging</li>
+          <li class="mt-1 text-gray-100">api integration</li>
+          <li class="mt-1 text-gray-100">responsive design</li>
         </ul>
       </div>
       <div class="w-full mt-10 sm:mt-0">
-        <h2 class="font-nunito uppercase text-lg text-indigo-500 tracking-wide mt-4">Languages and Frameworks</h2>
+        <h2 class="mt-4 text-lg tracking-wide text-indigo-500 uppercase font-nunito">Languages and Frameworks</h2>
           <ul class="">
-          <li class="text-gray-100 mt-1">Python</li>
-          <li class="text-gray-100 mt-1">HTML</li>
-          <li class="text-gray-100 mt-1">CSS</li>
-          <li class="text-gray-100 mt-1">Javascript</li>
-          <li class="text-gray-100 mt-1">Flask</li>
-          <li class="text-gray-100 mt-1">Django</li>
-          <li class="text-gray-100 mt-1">Vue</li>
-          <li class="text-gray-100 mt-1">Tailwind CSS</li>
+          <li class="mt-1 text-gray-100">Python</li>
+          <li class="mt-1 text-gray-100">HTML</li>
+          <li class="mt-1 text-gray-100">CSS</li>
+          <li class="mt-1 text-gray-100">Javascript</li>
+          <li class="mt-1 text-gray-100">Flask</li>
+          <li class="mt-1 text-gray-100">Django</li>
+          <li class="mt-1 text-gray-100">Vue</li>
+          <li class="mt-1 text-gray-100">Tailwind CSS</li>
         </ul>
       </div>
       </div>
-      <div class="flex items-center gap-9 mt-14">
+      <div class="items-center hidden sm:flex gap-9 mt-14">
         <a class="btn">Contact me</a>
-        <a href="/#work" class="bg-transparent border-2 hover:bg-indigo-700 hover:border-transparent
-      border-indigo-500 text-center  text-indigo-500 rounded-full h-14 w-14">
-        <i class="fa-solid mt-5 fa-chevron-down animate-bounce end 
-      text-indigo-500 hover:text-bg-gray-800">
+        <a href="/#work" class="text-center text-indigo-500 bg-transparent border-2 border-indigo-500 rounded-full hover:bg-indigo-700 hover:border-transparent h-14 w-14">
+        <i class="mt-5 text-indigo-500 fa-solid fa-chevron-down animate-bounce end hover:text-bg-gray-800">
         </i>
         </a>
       </div>
   </section>
 <!-- WORK SAMPLES --> 
 
-  <section id="work" class="section">
+  <section id="work" class="section flex xl:w-[calc(100%_+_10rem)] mr-0 mt-80 mb-64">
     <h1 class="text-5xl text-white mt-60">Work Samples</h1>
-  <div class="block md:flex my-20 md:gap-10 ">
-    <div class="w-full bg-gray-200 rounded-lg mb-5 shadow-md ">
+  <div class="block my-20 lg:flex lg:gap-20 ">
+    <div class="w-full mb-10 bg-gray-600 rounded-lg shadow-md xl:w-2/3 ">
         <a href="#">
-            <img class="rounded-t-lg object-cover h-30" src=".././assets/SLAS_dashboard.png" alt="" />
+            <img class="object-cover object-center w-full h-32 rounded-t-lg" src=".././assets/SLAS_dashboard.png" alt="" />
         </a>
-        <div class="p-5">
+        <div class="p-5 pb-10">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Flask Coffeeshop</h5>
+                <h5 class="mb-2 text-lg font-bold tracking-wide text-indigo-400 uppercase font-nunito">Flask Coffeeshop</h5>
             </a>
-            <p class="mb-8 font-normal text-gray-700 dark:text-gray-400">A coffee-ordering app, custom-designed in flask,
+            <p class="mb-8 font-normal text-gray-300 dark:text-gray-200">A coffee-ordering app, custom-designed in flask,
               for teachers to order a coffee with delivery from the Year 10 hospitality class.
               Authentication, ordering, keeping track of payments, order history, custom printed order labels, and more... </p>
-            <a href="http://cheryl.pythonanywhere.com" class="btn mt-4 mb-10">
+            <a href="http://cheryl.pythonanywhere.com" class="btn">
                 View Project
             </a>
         </div>
     </div>
-        <div class="w-full bg-gray-200 rounded-lg mb-5 shadow-md ">
+    
+        <div class="w-full mb-10 bg-gray-600 rounded-lg shadow-md xl:w-2/3 ">
         <a href="#">
-            <img class="rounded-t-lg object-cover h-32 w-full object-center" src=".././assets/cazz.png" alt="" />
+            <img class="object-cover object-center w-full h-32 rounded-t-lg" src=".././assets/cazz.png" alt="" />
         </a>
-        <div class="p-5">
+        <div class="p-5 pb-8">
             <a href="#">
-                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Crafting with Cazz</h5>
+                <h5 class="mb-2 text-lg font-bold tracking-wide text-indigo-400 uppercase font-nunito">Crafting with Cazz</h5>
             </a>
-            <p class="mb-8 font-normal text-gray-700 dark:text-gray-400">A blog made in Wordpress for a prolific crafting blogger and
+            <p class="mb-8 font-normal text-gray-300 dark:text-gray-400">A blog made in Wordpress for a prolific crafting blogger and
               card-maker. This was customised to make posting a simple process. She wanted a lot of photos in her blog posts and links back
               to her existing store. 
               
              </p>
-            <a href="https://craftingwithcazz.com/" class="btn mt-4">
+            <a href="https://craftingwithcazz.com/" class="btn">
                 View Project
             </a>
         </div>
@@ -128,22 +120,22 @@
 
   </section>
 
-<!-- Contacct me --> 
+<!-- Contact me --> 
 
-  <section id="contact" class="section mt-32">
-    <h1 class="text-5xl text-white mt-60 md:mt-20">Contact me</h1>
+  <section id="contact" class="mb-10 section">
+    <h1 class="text-5xl text-white">Contact me</h1>
     <form class="w-full max-w-xs mt-10" autocomplete="off">
       <div class="mb-4">
-        <label class="block text-gray-300 text-sm font-bold mb-2" for="name">Name:</label>
-        <input class="appearance-none bg-gray-600 focus:bg-gray-500 focus:border-1 focus:border-gray-600 rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none " type="text" id="name">
+        <label class="block mb-2 text-sm font-bold text-gray-300" for="name">Name:</label>
+        <input class="w-full px-3 py-2 leading-tight text-gray-300 bg-gray-600 rounded appearance-none focus:bg-gray-500 focus:border-1 focus:border-gray-600 focus:outline-none " type="text" id="name">
       </div>
       <div class="mb-4">
-        <label class="block text-gray-300 text-sm font-bold mb-2" for="email">Email:</label>
-        <input class="appearance-none bg-gray-600 focus:bg-gray-500 focus:border-1 focus:border-gray-600 rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none " type="email" id="email">
+        <label class="block mb-2 text-sm font-bold text-gray-300" for="email">Email:</label>
+        <input class="w-full px-3 py-2 leading-tight text-gray-300 bg-gray-600 rounded appearance-none focus:bg-gray-500 focus:border-1 focus:border-gray-600 focus:outline-none " type="email" id="email">
       </div>
           <div class="mb-4">
-        <label class="block text-gray-300 text-sm font-bold mb-2" for="message">Message</label>
-        <textarea class="appearance-none bg-gray-600 focus:bg-gray-500 focus:border-1 focus:border-gray-600 rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none " type="textarea" id="message" rows="6"></textarea>
+        <label class="block mb-2 text-sm font-bold text-gray-300" for="message">Message</label>
+        <textarea class="w-full px-3 py-2 leading-tight text-gray-300 bg-gray-600 rounded appearance-none focus:bg-gray-500 focus:border-1 focus:border-gray-600 focus:outline-none " type="textarea" id="message" rows="6"></textarea>
       </div>
       <button class="btn" type="submit">Submit</button>
     </form>
